b98c01be15bb34813bd02a962b28815b765e1a9e
Sven Andersson <sven.andersson@king.com>
2015-06-02 15:11:55 +0200

    Added copying of classes.dex and res folder for Android build.

--- b-plat.sh
+++ b-plat.sh
@@ -3,6 +3,7 @@ set -e
 P=$1
 Q=$2
 BOBDROP="./com.dynamo.cr/com.dynamo.cr.bob/libexec/$P"
+BOBLIBDROP="./com.dynamo.cr/com.dynamo.cr.bob/lib/"
 ENGDROP="./com.dynamo.cr/com.dynamo.cr.engine/engine/$Q"
 echo "Preparing for platform [$P] bobdrop to [$BOBDROP] enginedrop to [$ENGDROP]"
 ./scripts/build.py build_engine --platform=$P --skip-tests
@@ -10,3 +11,7 @@ mkdir -p "$BOBDROP"
 mkdir -p "$ENGDROP"
 cp "$DYNAMO_HOME/bin/$P"/* "$BOBDROP"
 cp "$DYNAMO_HOME/bin/$P"/* "$ENGDROP"
+mkdir -p "$BOBLIBDROP"
+mkdir -p "./com.dynamo.cr/com.dynamo.cr.target/res/"
+cp "$DYNAMO_HOME/share/java/classes.dex" "$BOBLIBDROP"
+cp -r "$DYNAMO_HOME/ext/share/java/res"/* "./com.dynamo.cr/com.dynamo.cr.target/res/"
