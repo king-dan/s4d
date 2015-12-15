17b800b7ac274dbd5bf717e4fba357789595f968
Dan Nilsson <dan.nilsson@king.com>
2015-12-15 09:44:00 +0100

    - fixed mixup

--- b-ios.sh
+++ b-ios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 MPATH=`dirname $BASH_SOURCE`
-"$MPATH/b-plat.sh" arm64-darwin ios
-"$MPATH/b-plat.sh" armv7-darwin arm64-ios
+"$MPATH/b-plat.sh" armv7-darwin ios
+"$MPATH/b-plat.sh" arm64-darwin arm64-ios
 
 
